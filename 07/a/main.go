bbb676500aa15a7268d828a746cbbe91044bcbad
Johannes Larsson <johannes.a.larsson@gmail.com>
2022-12-07 10:27:10 +0100

    day 7

--- 07/a/main.go
+++ 07/a/main.go
@@ -19,9 +19,9 @@ type FileDesc struct {
 	Size int
 }
 
-func getPartOne(rows []Instruction) int {
-	sizePerFolder := make(map[string]int, 0)
-	folderMap := make(map[string][]string, 0)
+func getSizePerFolderAndFolderChildRelation(rows []Instruction) (map[string]int, map[string][]string) {
+	localSizePerFolder := make(map[string]int, 0)
+	folderChild := make(map[string][]string, 0)
 	currentFolderStack := make([]string, 0)
 	for _, row := range rows {
 		size := 0
@@ -44,81 +44,55 @@ func getPartOne(rows []Instruction) int {
 		for _, file := range row.Files {
 			size += file.Size
 		}
+		localSizePerFolder[currentFolder] = size
+
 		for _, folder := range row.Folders {
-			folderMap[currentFolder] = append(folderMap[currentFolder], currentFolder+folder+"/")
-		}
-		sizePerFolder[currentFolder] = size
-	}
-
-	sum := 0
-	for folder := range sizePerFolder {
-		s := findFolderSize(folder, sizePerFolder, folderMap)
-		if s <= 100000 {
-			sum += s
+			folderChild[currentFolder] = append(folderChild[currentFolder], currentFolder+folder+"/")
 		}
 	}
-
-	return sum
+	return localSizePerFolder, folderChild
 }
 
-func findFolderSize(name string, sizePerFolder map[string]int, folderMap map[string][]string) int {
+func findActualFolderSize(name string, localSizePerFolder map[string]int, folderChild map[string][]string) int {
 	size := 0
-	if val, ok := sizePerFolder[name]; ok {
+	if val, ok := localSizePerFolder[name]; ok {
 		size += val
 	}
-	if _, ok := folderMap[name]; !ok {
+	if _, ok := folderChild[name]; !ok {
 		return size
 	}
-	for _, sub := range folderMap[name] {
-		size += findFolderSize(sub, sizePerFolder, folderMap)
+	for _, sub := range folderChild[name] {
+		size += findActualFolderSize(sub, localSizePerFolder, folderChild)
 	}
 	return size
 }
 
-func getPartTwo(rows []Instruction) int {
-	sizePerFolder := make(map[string]int, 0)
-	folderMap := make(map[string][]string, 0)
-	currentFolderStack := make([]string, 0)
-	for _, row := range rows {
-		size := 0
-		if strings.HasPrefix(row.Cmd, "cd ") {
-			if row.Cmd == "cd .." {
-				currentFolderStack = currentFolderStack[:len(currentFolderStack)-1]
-				continue
-			} else {
-				currentFolderStack = append(currentFolderStack, strings.Replace(row.Cmd, "cd ", "", 1))
-			}
+func getPartOne(rows []Instruction) int {
+	localSizePerFolder, folderChild := getSizePerFolderAndFolderChildRelation(rows)
+	sum := 0
+	for folder := range localSizePerFolder {
+		s := findActualFolderSize(folder, localSizePerFolder, folderChild)
+		if s <= 100000 {
+			sum += s
 		}
-		currentFolder := ""
-		for _, f := range currentFolderStack {
-			if f == "/" {
-				currentFolder += "/"
-			} else {
-				currentFolder += f + "/"
-			}
-		}
-		for _, file := range row.Files {
-			size += file.Size
-		}
-		for _, folder := range row.Folders {
-			folderMap[currentFolder] = append(folderMap[currentFolder], currentFolder+folder+"/")
-		}
-		sizePerFolder[currentFolder] = size
 	}
+	return sum
+}
 
+func getPartTwo(rows []Instruction) int {
+	localSizePerFolder, folderChild := getSizePerFolderAndFolderChildRelation(rows)
 	total := 70000000
 	wanted := 30000000
-	actual := findFolderSize("/", sizePerFolder, folderMap)
+	actual := findActualFolderSize("/", localSizePerFolder, folderChild)
 	res := make([]int, 0)
-	for folder := range sizePerFolder {
-		s := findFolderSize(folder, sizePerFolder, folderMap)
+	for folder := range localSizePerFolder {
+		s := findActualFolderSize(folder, localSizePerFolder, folderChild)
 		if total-actual+s > wanted {
 			res = append(res, s)
 		}
 	}
 
 	sort.Ints(res)
-
 	return res[0]
 }
 
@@ -158,8 +132,6 @@ func getRows(filename string) []Instruction {
 }
 
 func main() {
-	//printIf(4)
-	//printIf(14)
 	fmt.Println("Part one:", getPartOne(getRows("../input.txt")))
 	fmt.Println("Part two:", getPartTwo(getRows("../input.txt")))
 }

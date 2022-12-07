98f7cd12559e6a4d066482043e3d42f62083e452
Johannes Larsson <johannes.a.larsson@gmail.com>
2022-12-07 10:16:02 +0100

    First draft

---
+++ 07/a/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "testing"
+
+func TestFilePartOneTestCasesTest(t *testing.T) {
+	want := 95437
+	got := getPartOne(getRows("../test.txt"))
+	if got != want {
+		t.Errorf("test file one = %v; want %v", got, want)
+	}
+}
+
+func TestFilePartOneTestCasesInput(t *testing.T) {
+	want := 1315285 // 1005501 too low, 1359916 too high
+	got := getPartOne(getRows("../input.txt"))
+	if got != want {
+		t.Errorf("test file one = %v; want %v", got, want)
+	}
+}
+
+func TestFilePartTwoTestCasesTest(t *testing.T) {
+	want := 24933642
+	got := getPartTwo(getRows("../test.txt"))
+	if got != want {
+		t.Errorf("test file two = %v; want %v", got, want)
+	}
+}
+
+func TestFilePartTwoTestCasesInput(t *testing.T) {
+	want := 9847279
+	got := getPartTwo(getRows("../input.txt"))
+	if got != want {
+		t.Errorf("test file two = %v; want %v", got, want)
+	}
+}
+
+func BenchmarkPartOne(b *testing.B) {
+	// run the Fib function b.N times
+	row := getRows("../input.txt")
+	for n := 0; n < b.N; n++ {
+		getPartOne(row)
+	}
+}
+
+func BenchmarkPartTwo(b *testing.B) {
+	// run the Fib function b.N times
+	row := getRows("../input.txt")
+	for n := 0; n < b.N; n++ {
+		getPartTwo(row)
+	}
+}
